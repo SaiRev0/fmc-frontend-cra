dd2dd9546f85700aafb07d0c1ee359d6cd204ec8
SaiRev <saiyamjain1234567@gmail.com>
2022-12-14 21:42:04 +0530

    fixed

--- src/pages/Sponsors/SponsorCard.js
+++ src/pages/Sponsors/SponsorCard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const CardTitle = styled.h3`
   transform: translateZ(55px);
@@ -8,11 +8,12 @@ const CardTitle = styled.h3`
 function SponsorCard(props) {
   return (
     <div className="sponsor-card">
-      <div><a href={props.websiteLink} target="_blank">
-        <div className="sponsor_card_img">
-                    
-          <img src={props.img} alt="unicorn" className="sponsorcardImg"/>
-        </div></a>
+      <div>
+        <a href={props.websiteLink} rel="noreferrer" target="_blank">
+          <div className="sponsor_card_img">
+            <img src={props.img} alt="unicorn" className="sponsorcardImg" />
+          </div>
+        </a>
         <CardTitle className="cardTitle">{props.name}</CardTitle>
       </div>
 
